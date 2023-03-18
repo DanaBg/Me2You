407702cb22686532c68715bef0950f9f2a0d8de4
DanaBg <danusha115@gmail.com>
2023-03-18 21:00:28 +0200

    add profile page with logout and firebase data

--- app/src/main/java/com/example/me2you/ProfileFragment.java
+++ app/src/main/java/com/example/me2you/ProfileFragment.java
@@ -2,52 +2,98 @@ package com.example.me2you;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.me2you.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 
 public class ProfileFragment extends Fragment {
-    FragmentProfileBinding binding;
+    private FirebaseUser user;
+    private Button logoutButton;
+    private Button editButton;
+    private FragmentProfileBinding binding;
+    private String userDisplayName;
+    private String userEmail;
+    private String userPassword;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-//        binding.dateEditBtn.setOnClickListener((view)->{
-//            Dialog dialog = new DatePickerDialog(getContext(),(datePicker,yy,mm,dd)->{
-//                y = yy;
-//                m = mm;
-//                d = dd;
-//                setDate();
-//            },y,m,d);
-//            dialog.show();
-//        });
+        View root = binding.getRoot();
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        logoutButton = root.findViewById(R.id.logout);
+        editButton = root.findViewById(R.id.editProfile);
+        if(user != null) {
+            userEmail = user.getEmail();
+            userDisplayName = user.getDisplayName();
+
+            binding.email.setText(userEmail);
+            binding.profileName.setText(userDisplayName);
+            binding.password.setText("******");
+
+//            Uri imageUri = Uri.parse("https://cdn.primedia.co.za/primedia-broadcasting/image/upload/c_fill,h_436,w_700/ykcxncolmdjmafvnznrm");
 //
-//        binding.dateInputEt.setOnTouchListener(new View.OnTouchListener() {
+//            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+//                    .setPhotoUri(imageUri)
+//                    .build();
+//
+//            user.updateProfile(profileUpdates);
+
+            Handler hand = new Handler();
+            hand.postDelayed(setText(),100);
+            if(user.getPhotoUrl() != null) {
+                Picasso.get().load(user.getPhotoUrl()).into(binding.profileImg);
+            }
+        }
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user != null){
+                    FirebaseAuth.getInstance().signOut();
+                    Intent i = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+//        editButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-//                    Dialog dialog = new DatePickerDialog(getContext(),(datePicker,yy,mm,dd)->{
-//                        y = yy;
-//                        m = mm;
-//                        d = dd;
-//                        setDate2();
-//                    },y,m,d);
-//                    dialog.show();
-//                    return true;
-//                }
-//                return false;
+//            public void onClick(View v) {
+//
+//                ProfileFragmentDirections.ActionNavigationProfileToEditProfile action =
+//                        ProfileFragmentDirections.actionNavigationProfileToEditProfile(user.getDisplayName(),
+//                                user.getPhotoUrl());
+//
+//                Navigation.findNavController(v).navigate(action);
 //            }
 //        });
-        return binding.getRoot();
+        return root;
+    }
+    public Runnable setText(){
+        return new Runnable() {
+            @Override
+            public void run() {
+                binding.profileName.setText(user.getDisplayName());
+            }
+        };
     }
 }
